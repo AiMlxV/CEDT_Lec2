331a912d767490c58f90210ac0cc303b357b279e
AiMlxV <41025@chs.ac.th>
2026-01-14 09:37:22 +0700

    add: new class MySchool

--- src/Student/MySchool.java
+++ src/Student/MySchool.java
@@ -1,4 +1,9 @@
 package Student;
 
 public class MySchool {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.setName("Toe");
+        System.out.println(s1.getName());
+    }
 }
